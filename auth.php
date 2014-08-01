1230d0d80117e079df30aae2682abe50e66e5247
Joseph Constantakis <jcon5294@gmail.com>
2014-08-01 15:23:39 -0700

    add php files

---
+++ auth.php
@@ -0,0 +1,29 @@
+<?php
+require 'vendor/autoload.php';
+
+// AccountSid and AuthToken can be found in your account dashboard
+$accountSid = "REPLACE_WITH_ACCOUNT_SID"; 
+$authToken = "REPLACE_WITH_AUTH_TOKEN"; 
+
+// The app outgoing connections will use:
+$appSid = "REPLACE_WITH_APP_SID"; 
+
+// The client name for incoming connections:
+$clientName = "monkey";
+
+$capability = new Services_Twilio_Capability($accountSid, $authToken);
+
+// This allows incoming connections as $clientName: 
+$capability->allowClientIncoming($clientName);
+
+// This allows outgoing connections to $appSid with the "From" 
+// parameter being the value of $clientName 
+$capability->allowClientOutgoing($appSid, array(), $clientName);
+
+// This returns a token to use with Twilio based on 
+// the account and capabilities defined above 
+$token = $capability->generateToken();
+
+echo $token;
+?>
+
