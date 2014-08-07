d2e8e5a322c454bb4b0efc391e8232029cf537db
Joseph Constantakis <jcon5294@gmail.com>
2014-08-07 16:33:33 -0700

    update auth.php to return json and take client name from request param

--- auth.php
+++ auth.php
@@ -1,22 +1,24 @@
 <?php
 require 'vendor/autoload.php';
 
+header('Content-Type: application/json');
+
 // AccountSid and AuthToken can be found in your account dashboard
-$accountSid = "REPLACE_WITH_ACCOUNT_SID"; 
-$authToken = "REPLACE_WITH_AUTH_TOKEN"; 
+$accountSid = 'REPLACE_WITH_ACCOUNT_SID'; 
+$authToken = 'REPLACE_WITH_AUTH_TOKEN'; 
 
 // The app outgoing connections will use:
-$appSid = "REPLACE_WITH_APP_SID"; 
+$appSid = 'REPLACE_WITH_APP_SID'; 
 
 // The client name for incoming connections:
-$clientName = "monkey";
+$clientName = $_REQUEST['clientName'] ?: 'monkey';
 
 $capability = new Services_Twilio_Capability($accountSid, $authToken);
 
 // This allows incoming connections as $clientName: 
 $capability->allowClientIncoming($clientName);
 
-// This allows outgoing connections to $appSid with the "From" 
+// This allows outgoing connections to $appSid with the 'From' 
 // parameter being the value of $clientName 
 $capability->allowClientOutgoing($appSid, array(), $clientName);
 
@@ -24,6 +26,5 @@ $capability->allowClientOutgoing($appSid, array(), $clientName);
 // the account and capabilities defined above 
 $token = $capability->generateToken();
 
-echo $token;
+echo json_encode(array('token'=>$token));
 ?>
-
